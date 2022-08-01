2253af2f51eb7b3f0a706f2a2a679b343a7f5fc6
c4lyp5o <calypso.hellhammer@gmail.com>
2022-08-01 10:13:50 +0800

    added carousel, scrolling banner, solat times and entrypoint

---
+++ pages/entrypoint.js
@@ -0,0 +1,13 @@
+import ScrollingBanner from "../components/ScrollingBanner";
+
+function Entrypoint() {
+  return (
+      <>
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a simple example of a React component.</p>
+    </div>
+      <ScrollingBanner />
+      </>
+  );
+}
